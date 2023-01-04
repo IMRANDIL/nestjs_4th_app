6058eaa0e99f9af8fe76916aa81e3d48b7714063
IMRANDIL <aliimranadil2@gmail.com>
2023-01-04 16:35:22 +0530

    some more change

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, Body, Param } from '@nestjs/common';
+import { createUserDTO } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
   @Post('/signup')
-  createUser() {}
+  createUser(@Body() body: createUserDTO) {}
 }
