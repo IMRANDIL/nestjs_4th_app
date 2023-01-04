b76dfc306d6de87cfd16a9bcb065e8df0cc9289d
IMRANDIL <aliimranadil2@gmail.com>
2023-01-04 21:19:01 +0530

    sssss

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -8,7 +8,19 @@ import { createUserDTO } from './dtos/create-user.dto';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   async createUser(email: string, password: string) {
-    // const findUser = await this.repo.findOne({email:email});
+    const findUser = await this.repo.findOneBy({ email: email });
+    if (findUser) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'User Already Existed',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: null,
+        },
+      );
+    }
     const createNewUser = this.repo.create({ email, password });
 
     return this.repo.save(createNewUser);
