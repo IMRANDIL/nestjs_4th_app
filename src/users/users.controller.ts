76fce045902b8d805dcbc41f59c40bab68b355d5
IMRANDIL <aliimranadil2@gmail.com>
2023-01-05 16:54:35 +0530

    some more improvement

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,25 +12,25 @@ import {
 import { createUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/Interceptors/serialize.interceptor';
+import { Serialize } from 'src/Interceptors/serialize.interceptor';
 import { UserDTO } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Get('/allUser')
   getAllUser() {
     return this.userService.getAllUser();
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Post('/signup')
   createUser(@Body() body: createUserDTO) {
     return this.userService.createUser(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Get('/:id')
   async getSpecificUser(@Param('id') id: string) {
     const specificUser = await this.userService.getSpecificUser(parseInt(id));
@@ -40,13 +40,13 @@ export class UsersController {
     return specificUser;
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Put('/:id')
   update(@Param('id') id: string, @Body() body: UpdateUserDTO) {
     return this.userService.update(parseInt(id), body);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Delete('/:id')
   delete(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
