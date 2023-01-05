01e48fb7f718f7e1c902800efff3d94c5cfedf26
IMRANDIL <aliimranadil2@gmail.com>
2023-01-05 16:50:28 +0530

    some more change

--- src/Interceptors/serialize.interceptor.ts
+++ src/Interceptors/serialize.interceptor.ts
@@ -7,9 +7,9 @@ import {
 import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UserDTO } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     //Run something before a request is handled by the request handler
     // console.log('I am running before the request handler', context);
@@ -18,7 +18,7 @@ export class SerializeInterceptor implements NestInterceptor {
       map((data: any) => {
         //Run something before the response is sent out
         // console.log('I am running before the response is sent out', data);
-        return plainToClass(UserDTO, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,23 +13,24 @@ import { createUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { SerializeInterceptor } from 'src/Interceptors/serialize.interceptor';
+import { UserDTO } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDTO))
   @Get('/allUser')
   getAllUser() {
     return this.userService.getAllUser();
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDTO))
   @Post('/signup')
   createUser(@Body() body: createUserDTO) {
     return this.userService.createUser(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDTO))
   @Get('/:id')
   async getSpecificUser(@Param('id') id: string) {
     const specificUser = await this.userService.getSpecificUser(parseInt(id));
@@ -39,13 +40,13 @@ export class UsersController {
     return specificUser;
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDTO))
   @Put('/:id')
   update(@Param('id') id: string, @Body() body: UpdateUserDTO) {
     return this.userService.update(parseInt(id), body);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDTO))
   @Delete('/:id')
   delete(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
