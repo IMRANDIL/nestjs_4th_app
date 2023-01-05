89557070f46bd8ba43f5f2c5854c0f5d8377dc3c
IMRANDIL <aliimranadil2@gmail.com>
2023-01-05 09:43:57 +0530

    remove done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -51,5 +51,11 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repo.remove(user);
+  }
 }
