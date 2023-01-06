23e7d5ada2aa11ce1446043e2db7470b83e7bf27
IMRANDIL <aliimranadil2@gmail.com>
2023-01-06 09:47:15 +0530

    some more improve

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -31,7 +31,7 @@ export class UsersController {
 
   @Post('/signup')
   createUser(@Body() body: createUserDTO) {
-    return this.userService.createUser(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
 
   @Get('/:id')
