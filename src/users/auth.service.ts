c5ab4d07dc10f489320dbdbf8ac8246d05699fff
IMRANDIL <aliimranadil2@gmail.com>
2023-01-06 09:43:40 +0530

    some more

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
@@ -9,5 +12,21 @@ export class AuthService {
     if (user) {
       throw new BadRequestException('User already Exist');
     }
+
+    //generate the salt..
+    const salt = randomBytes(8).toString('hex');
+
+    //add hashed password
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    //join hash and salt
+
+    const result = hash.toString('hex') + '.' + salt;
+
+    // create a new user..
+
+    const newUser = await this.usersService.createUser(email, result);
+
+    return newUser;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,19 +12,6 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   async createUser(email: string, password: string) {
-    const findUser = await this.repo.findOneBy({ email: email });
-    if (findUser) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          error: 'User Already Existed',
-        },
-        HttpStatus.FORBIDDEN,
-        {
-          cause: null,
-        },
-      );
-    }
     const createNewUser = this.repo.create({ email, password });
 
     return this.repo.save(createNewUser);
