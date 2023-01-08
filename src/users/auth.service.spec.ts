e291dd9c02776baca8c869d859b5940141155048
IMRANDIL <aliimranadil2@gmail.com>
2023-01-08 11:51:51 +0530

    some more

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,8 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
+  //create a fake copy of users service
+
+  const fakeUsersService: Partial<UsersService> = {
+    findUser: () => Promise.resolve({} as User),
+    createUser: () => Promise.resolve({} as User),
+  };
+
   const module = await Test.createTestingModule({
     providers: [AuthService],
   }).compile();
