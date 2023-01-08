07e1dc942de6f3bdc56379355e5355c6fb1f36a1
IMRANDIL <aliimranadil2@gmail.com>
2023-01-08 11:42:53 +0530

    some more added

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -6,4 +6,8 @@ it('can create an instance of auth service', async () => {
   const module = await Test.createTestingModule({
     providers: [AuthService],
   }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
 });
