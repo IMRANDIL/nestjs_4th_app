c8f080d850f84aedf9d701c583bf000d31df0f4e
IMRANDIL <aliimranadil2@gmail.com>
2023-01-08 13:02:56 +0530

    some more

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService in Action', () => {
   let service: AuthService;
@@ -13,7 +14,7 @@ describe('AuthService in Action', () => {
       findUser: (email: string) =>
         Promise.resolve({ id: 1, email, password: '2580123' } as User),
       createUser: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+        Promise.resolve({ id: 2, email, password } as User),
     };
 
     const module = await Test.createTestingModule({
@@ -28,5 +29,19 @@ describe('AuthService in Action', () => {
 
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    await expect(service.signup('ali@gmail.com', '2580123')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('raju@gmail.com', '2580123');
+    expect(user.password).not.toEqual('2580123');
+    const [hash, salt] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
